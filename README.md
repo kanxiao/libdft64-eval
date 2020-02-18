fec6624cd8a96ea7d301d57a0bcaa236a6b52fc6
ubuntu-16.04 <suncong.pku@gmail.com>
2020-02-17 23:50:02 -0800

    bench update

---
+++ README.md
@@ -0,0 +1,15 @@
+# Benchmarks of fast-libdft64.
+
+SPEC CPU 2006 benchmarks as well as an nginx server for evaluating the fast-libdft64.
+
+## Build
+
+Run
+
+```
+./batch-make.sh
+```
+
+The binaries are in the *src/* or *bin/* sub-directory of each benchmark.
+
+
---
+++ nginx/build-nginx.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+work_path=$(dirname $0)
+cd ./${work_path}
+nginx_root=$(pwd)
+
+echo $nginx_root
+
+nginx_target_dir=$nginx_root"/bin"
+
+nginx_src_dir=$nginx_root"/lib/nginx-1.16.0"
+pcre_dir=$nginx_root"/lib/pcre-8.43"
+zlib_dir=$nginx_root"/lib/zlib-1.2.11"
+
+echo $nginx_target_dir
+echo $nginx_src_dir
+
+cd $nginx_src_dir
+
+./configure --with-cc="gcc" --with-cc-opt="-O0 -mno-avx -fno-stack-protector" --with-pcre=$pcre_dir --with-pcre-opt="-g -O0 -mno-avx -fno-stack-protector" --with-zlib=$zlib_dir --with-zlib-opt="-g -O0 -mno-avx -fno-stack-protector"
+
+make
+
+mv $nginx_src_dir"/objs/nginx" $nginx_target_dir"/nginx_gcc"
+
+objdump -d $nginx_target_dir"/nginx_gcc" > $nginx_target_dir"/nginx_gcc.asm"
